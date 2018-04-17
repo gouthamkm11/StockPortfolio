adb235d0c0a4a1d1a873d6684d4839b6c741d45f
Goutham <gouthamcold@gmail.com>
2018-04-16 22:56:11 -0400

    angular material changes

--- src/app/features/user-profile/user-profile.component.ts
+++ src/app/features/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { userProfileServices } from '../../services/userProfile.services';
 import { userProfile } from '../../models/userProfile.model';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-user-profile',
@@ -23,6 +24,16 @@ export class UserProfileComponent implements OnInit {
       this.routing.profileDetailsEmitter.emit(this.userProfileDetails);
     });
   }
+
+  //layout Design
+  tiles = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ]
+
+
 
   //Source for all profile values
   
