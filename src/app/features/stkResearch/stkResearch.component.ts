735cc992f9886d0e75ca53dd736ffc09b454e778
Goutham <gouthamcold@gmail.com>
2018-04-20 08:27:47 -0400

    wathclist module completed

--- src/app/features/stkResearch/stkResearch.component.ts
+++ src/app/features/stkResearch/stkResearch.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { stkRoutingServices } from "../../services/stkRouting.services";
+import { userProfileServices } from "../../services/userProfile.services";
 
 @Component({
     selector: 'app-stkResearch',
     templateUrl: './stkResearch.component.html',
     styleUrls: ['./stkResearch.component.css'],
-    providers: [stkRoutingServices]
+    providers: [stkRoutingServices,userProfileServices]
 })
 
 export class stkResearchComponent{
--- src/app/features/stkResearch/stkSearch/stkSearch.component.ts
+++ src/app/features/stkResearch/stkSearch/stkSearch.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { stkRoutingServices } from '../../../services/stkRouting.services';
+import { userProfileServices } from '../../../services/userProfile.services';
+import { stkWathclist } from '../../../models/stkWatchlist.model';
+import { Symbol } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-stkSearch',
   templateUrl: './stkSearch.component.html',
-  styleUrls: ['./stkSearch.component.css'],
+  styleUrls: ['./stkSearch.component.css']
 })
 export class stkSearchComponent implements OnInit {
 
-  constructor(private routing:stkRoutingServices) { }
+  constructor(private routing:stkRoutingServices,
+    private _userProfileService:userProfileServices,
+    private _http:HttpClient
+  ) { }
 
   symbol:string;
+  list;
+  watchlistSymbols;
 
   ngOnInit() {
   }
@@ -23,5 +33,21 @@ export class stkSearchComponent implements OnInit {
     this.routing.symbolEmitter.emit(this.symbol);
     this.routing.getStkAbout(this.symbol).subscribe(res =>{this.routing.descriptionEmitter.emit(res);});
     this.routing.getStkStats(this.symbol).subscribe(res => {this.routing.statsEmitter.emit(res);});
+  }
+
+  addToWatchlist(){
+    this._userProfileService.getStkWatchlistDetails().subscribe((symbol)=>{
+      this.list= symbol;
+      this.watchlistSymbols = this.list.stocks;
+      //checking whether the symbol present in watchlist
+      let ans = this.watchlistSymbols.indexOf(this.symbol);
+      if(ans === -1){
+        this._http.post('http://localhost:3002/api/watchlistData/',{
+          googleID:14,
+          stocks:`${this.symbol}`
+        }).subscribe((res)=>res);
+      }
+      else {console.log("stock exits")};
+    });
   }
 }
